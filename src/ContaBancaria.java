4739664b8ec4c9b0128c139988a65adceb72da94
DiogoAsenjo <diogoasenjo@outlook.com>
2023-09-12 09:50:53 -0300

    Testando novos métodos

--- src/ContaBancaria.java
+++ src/ContaBancaria.java
@@ -4,7 +4,7 @@ public class ContaBancaria {
     String cpf;
     double saldo;
 
-    void depositar(double valor) {
+    public void depositar(double valor) {
         this.saldo += valor;
         System.out.println("""
                 Depósito realizado!
@@ -12,13 +12,26 @@ public class ContaBancaria {
                 """.formatted(this.saldo));
     }
 
-    void sacar(double valor) {
+    public void sacar(double valor) {
         if (this.saldo < valor) {
             System.out.println("Saldo insuficiente");
         } else {
             this.saldo -= valor;
             System.out.println("""
                 Saque efetuado!
+                Saldo atualizado: %.2f
+                """.formatted(this.saldo));
+        }
+    }
+
+    public void transferir(double valor, ContaBancaria destino) {
+        if (this.saldo < valor) {
+            System.out.println("Saldo insuficiente");
+        } else {
+            this.saldo -= valor;
+            destino.saldo += valor;
+            System.out.println("""
+                Transferência efetuada!
                 Saldo atualizado: %.2f
                 """.formatted(this.saldo));
         }
--- src/UsandoConta.java
+++ src/UsandoConta.java
@@ -7,13 +7,21 @@ public class UsandoConta {
         primeiraConta.cpf = "12345678901";
         primeiraConta.saldo = 200.76;
 
+        ContaBancaria segundaConta = new ContaBancaria();
+        segundaConta.numeroConta = 0002;
+        segundaConta.titular = "Neemias Vieira";
+        segundaConta.cpf = "12345678901";
+        segundaConta.saldo = 400;
+
         //REFERÊNCIA
         /*ContaBancaria referenciaConta = primeiraConta;
         referenciaConta.saldo += 100;
         System.out.println(primeiraConta.saldo);*/ //O output aqui será 300.76. Isso porque referenciaConta não é um novo objeto. Mas sim uma referência ao objeto primeiraConta.
 
         //CHAMANDO OS MÉTODOS
-        primeiraConta.depositar(100.50);
-        primeiraConta.sacar(10);
+        //primeiraConta.depositar(100.50);
+        //primeiraConta.sacar(10);
+        primeiraConta.transferir(10, segundaConta);
+        System.out.println(segundaConta.saldo);
     }
 }
